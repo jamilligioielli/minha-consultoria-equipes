00b16944ffbc3afcc6a0e583dbf4e2fe77d7cd89
Jamilli <jamilligioielli@outlook.com>
2024-09-16 22:02:30 -0300

    remove cidade.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { LiderModule } from './controllers/lider/lider.module';
 import oracleConfig from 'config/oracle.config';
-import { CidadeModule } from './models/cidade/cidade.module';
 
 @Module({
   imports: [
@@ -14,7 +13,6 @@ import { CidadeModule } from './models/cidade/cidade.module';
       envFilePath: '.env',
     }),
     LiderModule,
-    CidadeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
